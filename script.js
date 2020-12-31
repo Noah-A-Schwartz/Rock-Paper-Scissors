a8683eb8a5c8318399e73353a32cb9f129b3bd94
Noah Schwartz <noah.amir.schwartz@gmail.com>
2020-12-30 20:15:47 -0500

    Added Styles

--- script.js
+++ script.js
@@ -7,58 +7,75 @@ function computerPlay(){
     }
 
 function playRound(playerMove, computerMove){
-    document.getElementsByTagName("P")[0].textContent = "You play " + playerMove + "\t Computer plays " + computerMove;
+    document.getElementsByTagName("P")[0].textContent = "You play " + playerMove + " and Computer plays " + computerMove;
     console.log("You play " + playerMove)
     console.log("Computer plays " + computerMove);
     if (playerMove === computerMove)
-         return "Tie";
+         return -1;
     else if (playerMove == "rock"){
         if (computerMove == "paper"){
             computerScore++;
-            return "Computer Wins round " + round;
+            return 1;
         }
         else if(computerMove === "scissors"){
             playerScore++;
-            return "Player wins round " + round;
+            return 0;
         }
     }
     else if (playerMove == "paper"){
         if (computerMove == "rock"){
             playerScore++;
-            return "Player Wins round " + round;
+            return 0;
         }
         else if(computerMove === "scissors"){
             computerScore++;
-            return "Computer wins round " + round;  
+            return 1  ;
         }
     }
     else if (playerMove == "scissors"){
         if (computerMove == "paper"){
             playerScore++;
-            return "Player Wins round " + round; 
+            return 0;
         }
         else if(computerMove === "rock"){
             computerScore++;
-            return "Computer wins round " + round;
+            return 1;
         }
     }
       
 }
 function getPlayerMove(button){
     let playerMove = button.value;
-    console.log(playRound(playerMove, computerPlay()));
-    updateGame(); 
+    let computerMove = computerPlay();
+    let result = playRound(playerMove, computerMove);
+    updateGame(result); 
 }
 function showButtons(){
+        textAreas = document.getElementsByTagName("DIV");
+        textAreas[4].appendChild(document.createElement("p"));
+        textAreas[5].appendChild(document.createElement("p"));
+        textAreas[6].appendChild(document.createElement("p"));
+        resetGame();
         let div = document.getElementsByClassName("play-buttons")[0];
         div.childNodes.forEach(function(button){
             if(button.tagName == "BUTTON")
             button.style.visibility="visible";
         }); 
+        button = document.getElementsByTagName("BUTTON")[0];
+        button.disabled = "true";
 }
 
-function updateGame(){
-        document.getElementsByTagName("P")[1].textContent ="Scores after round " + round + ":\n" + "Player Score: " + playerScore + "\n" + "Computer Score: " + computerScore + "\n"; 
+function updateGame(result){
+        document.getElementsByTagName("P")[1].textContent ="Scores after round " +  round + ": ";
+        document.getElementsByTagName("P")[2].textContent = "Player: " + playerScore + "\n" + "Computer: " + computerScore;
+        if(result == 0){
+            document.getElementById("flashtext").textContent = "YOU WIN THIS ROUND!"
+        }
+        else if(result == 1){
+            document.getElementById("flashtext").textContent = "YOU LOSE THIS ROUND!"
+        }
+        else document.getElementById("flashtext").textContent = "ITS A TIE";
+        document.getElementById("flashtext").style.visibility = "visible";
         console.log("Scores after round " + round + ":")
         console.log("Player Score: " + playerScore);
         console.log("Computer Score: " + computerScore);
@@ -76,9 +93,11 @@ function updateGame(){
 
 function resetGame(){
     //reset game
-    document.getElementsByTagName("P")[0].textContent = "";
-    document.getElementsByTagName("P")[1].textContent = "";
     round = 1;
     playerScore = 0;
     computerScore = 0;
+    document.getElementsByTagName("P")[0].textContent = "";
+    document.getElementsByTagName("P")[1].textContent = "";
+    document.getElementsByTagName("P")[2].textContent = "Player: " + playerScore + "\n" + "Computer: " + computerScore;
+    document.getElementById("flashtext").style.visibility = "hidden";
 }
